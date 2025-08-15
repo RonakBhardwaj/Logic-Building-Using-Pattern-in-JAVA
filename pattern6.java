a22b83a8218f2bd8cd26eeb6c0ae6871e3ce5e4e
RonakBhardwaj <ronakbh949471@gmail.com>
2025-08-15 19:15:15 +0530

    Pattern 7 added

--- pattern6.java
+++ pattern6.java
@@ -3,8 +3,13 @@ import java.util.Scanner;
 public class pattern6 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.printl("Please Enter The Value");
+        System.out.print("Please Enter The Value : ");
         int n= input.nextInt();
-        for(int i=)
+        for(int i = n;i > 0;i--){
+            for(int j = 1;j <= i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
     }
 }
